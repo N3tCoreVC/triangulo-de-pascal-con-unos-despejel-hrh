f1edfab9de6b17bf9f3703768bf68d2d18f55f00
Daniel Espejel <dkprogramacion@gmail.com>
2018-09-21 18:00:57 -0500

    Codigo actualizado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,50 @@ namespace TrianguloPascal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            Console.Write("Cuantas filas desea tener en su triangulo? -->  ");
+            Console.ForegroundColor = ConsoleColor.White;
+            int buffer;
+            if (int.TryParse(Console.ReadLine(), out buffer))
+            {
+                PrintShape(buffer);
+            }
+            else
+            {
+                Console.WriteLine("Por favor ingrese solo números enteros...");
+                Console.WriteLine();  
+            }
+        }
+
+        static void PrintShape(int target)
+        {
+            System.Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.Write("\n\n\n\n");
+
+            int buffer= target-1;
+
+            for(int iterator=1; iterator<=target; iterator++)
+            {
+                for(int innerIterator=0; innerIterator<buffer; innerIterator++)
+                {
+                    Console.Write(" ");
+                }
+                
+                for(int auxIterator=0; auxIterator<iterator; auxIterator++)
+                {
+                    Console.Write("1 ");
+                }
+                Console.Write("\n");
+                buffer--;
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\n\n\n\n");
         }
     }
 }
+
